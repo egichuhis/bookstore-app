f5665a248fb48d7202e9794d057aa6f1b56fdb27
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-09-27 11:19:28 +0300

    Add prop-types for validation

--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BookCard from './BookCard';
 
-const BooksList = () => (
-  <BookCard />
-);
+const BooksList = () => {
+  const myBooks = useSelector((state) => state.books.booksLibrary);
+
+  return (
+    <div>
+      {myBooks.map((book) => (
+        <ul key={book.id}>
+          <li>
+            <BookCard book={book} />
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+};
 
 export default BooksList;
