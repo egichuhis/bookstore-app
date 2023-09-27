1fdf7521d7fc99e1c8b412dd1a79c076ce55b2b9
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-09-27 11:55:19 +0300

    Add book and update global state

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,48 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/features/books/booksSlice';
 
-const AddBook = () => (
-  <div className="card mt-4" style={{ border: 'none' }}>
-    <div className="container">
-      <h4 style={{ color: 'grey', fontSize: '16.4px' }}>ADD NEW BOOK</h4>
-      <div className="row">
-        <div
-          className="col-6 col-md-4 align-self-center"
-          style={{ width: '50%' }}
-        >
-          <input type="text" placeholder="Book Title" style={{ width: '100%' }} />
-        </div>
-        <div className="col-md-4 align-self-center" style={{ width: '25%' }}>
-          <select style={{ width: '100%' }}>
-            <optgroup label="Author" placeholder="Author">
-              <option value={12} selected="">
-                Oprah Winfrey
-              </option>
-              <option value={13}>Jeffery Archer</option>
-              <option value={14}>James Paterson</option>
-            </optgroup>
-          </select>
-        </div>
-        <div
-          className="col-md-4 text-end align-self-center"
-          style={{ width: '25%' }}
-        >
-          <button
-            className="btn btn-primary btn-sm"
-            type="button"
-            style={{
-              fontSize: 14,
-              background: '#0290ff',
-              paddingRight: 34,
-              paddingLeft: 34,
-            }}
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const [bookDetails, setBookDetails] = useState({
+    title: '',
+    author: '',
+  });
+
+  return (
+    <div className="card mt-4" style={{ border: 'none' }}>
+      <div className="container">
+        <h4 style={{ color: 'grey', fontSize: '16.4px' }}>ADD NEW BOOK</h4>
+        <div className="row">
+          <div
+            className="col-6 col-md-4 align-self-center"
+            style={{ width: '50%' }}
           >
-            ADD BOOK
-          </button>
+            <input
+              type="text"
+              value={bookDetails.title}
+              onChange={(e) => setBookDetails({ ...bookDetails, title: e.target.value })}
+              placeholder="Book Title"
+              style={{ width: '100%' }}
+            />
+          </div>
+          <div
+            className="col-6 col-md-4 align-self-center"
+            style={{ width: '25%' }}
+          >
+            <input
+              type="text"
+              value={bookDetails.author}
+              onChange={(e) => setBookDetails({ ...bookDetails, author: e.target.value })}
+              placeholder="Author"
+              style={{ width: '100%' }}
+            />
+          </div>
+          <div
+            className="col-md-4 text-end align-self-center"
+            style={{ width: '25%' }}
+          >
+            <button
+              onClick={() => dispatch(addBook(bookDetails))}
+              className="btn btn-primary btn-sm"
+              type="button"
+              style={{
+                fontSize: 14,
+                background: '#0290ff',
+                paddingRight: 34,
+                paddingLeft: 34,
+              }}
+            >
+              ADD BOOK
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 
-);
+  );
+};
 
 export default AddBook;
--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types'; 
+import PropTypes from 'prop-types';
 import Chart from './charts/DonutChart';
 
 const BookCard = ({ book }) => {
